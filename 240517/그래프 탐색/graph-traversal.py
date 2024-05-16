13decda3ade7c56de5595c7dfe4418a91301e143
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-05-17 02:14:10 +0900

    Title: 그래프 탐색, Time: 215ms, Memory: 33MB, Status: Passed - CodeTree

--- 240517/그래프 탐색/graph-traversal.py
+++ 240517/그래프 탐색/graph-traversal.py
@@ -1,19 +1,19 @@
 N, M = list(map(int, input().split()))
-graph = [[0 for _ in range(N)] for _ in range(N)]
-visited = [False for _ in range(N)]
+graph = [[0 for _ in range(N+1)] for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
 cnt = 0
 
 for _ in range(M):
     x, y = list(map(int, input().split()))
-    graph[x-1][y-1] = 1
-    graph[y-1][x-1] = 1
+    graph[x][y] = 1
+    graph[y][x] = 1
 
 def DFS(vertex):
     global cnt
-    for curr_v in range(1, M):
+    visited[vertex] = True
+    for curr_v in range(1, N+1):
         if graph[vertex][curr_v] and not visited[curr_v]:
             cnt += 1
-            visited[curr_v] = True
             DFS(curr_v)
 
 DFS(1)
