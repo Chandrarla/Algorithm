daea7ff08957263668b5a5062f4306b2ab13d49f
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-10-12 19:47:40 +0900

    캐슬 디펜스(삼성 A형 기출 문제)

---
+++ BOJ/17136.py
@@ -0,0 +1,37 @@
+grid = [list(map(int, input().split())) for _ in range(10)]
+visited = [[0] * 10 for _ in range(10)]
+cnt = 0
+
+def can_place(x, y, s):
+    for i in range(x, x+s):
+        for j in range(y, y+s):
+            if not grid[i][j]:
+                return False
+            if visited[i][j]:
+               return False
+
+    return True
+
+def in_range(x, y, s):
+    return 0 <= x + s-1 < 10 and 0 <= y + s-1 < 10
+
+def check_visited(x, y, s):
+    for i in range(x, x+s):
+        for j in range(y, y+s):
+            visited[i][j] = 1
+
+
+for s in range(5, 0, -1):
+    paper = 0
+    for x in range(10):
+        for y in range(10):
+            if paper <= 5 and in_range(x, y, s) and can_place(x, y, s):
+                check_visited(x, y, s)
+                paper += 1
+                cnt += 1
+
+if visited != grid:
+    print(-1)
+else: print(cnt)
+
+
